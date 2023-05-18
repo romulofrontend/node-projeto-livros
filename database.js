a3f918abff9581370e0070f76910c5ae6a8df689
Romulo Rosa <romulo.frontend@gmail.com>
2023-05-18 18:20:42 -0300

    database criada e exportada

---
+++ database.js
@@ -0,0 +1,58 @@
+const livros = [
+  {
+    id : 1,
+    titulo: "Primeiros passos com a linguagem Rust",
+    autor: "José Augusto N. G. Manzano",
+    categoria: "Programação",
+    paginas: 312,
+    recomenda: true,
+    lido: true
+  },
+  {
+    id : 2,
+    titulo: "Programação em Baixo Nível",
+    autor: "Igor Zhirkov",
+    categoria: "Hardware &amp; Robótica",
+    paginas: 576,
+    recomenda: false,
+    lido: false
+  },
+  {
+    id : 3,
+    titulo: "Introdução à linguagem Lua",
+    autor: "José Augusto N. G. Manzano",
+    categoria: "Programação",
+    paginas: "176",
+    recomenda: true,
+    lido: false
+  },
+  {
+    id : 4,
+    titulo: "Lógica de Programação e Algoritmos com JavaScript",
+    autor: "Edécio Fernando Iepsen",
+    categoria: "Programação",
+    paginas: 320,
+    recomenda: false,
+    lido: false
+  },
+  {
+    id : 5,
+    titulo: "A Linguagem de Programação Go",
+    autor: "Alan A. Donovan",
+    categoria: "Programação",
+    paginas: 480,
+    recomenda: false,
+    lido: false
+  },
+  {
+    id : 6,
+    titulo: "Padrões Javscript",
+    autor: "Stoyan Stefanov",
+    categoria: "Programação",
+    paginas: 231,
+    recomenda: true,
+    lido: true
+  }
+]
+
+module.exports = livros
